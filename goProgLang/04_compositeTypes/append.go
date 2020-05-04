deaf9166944f932fc62ec1dfe19ec0d78afc790e
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-05-03 17:42:21 -0700

    +append, rev

---
+++ goProgLang/04_compositeTypes/append.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func appendInt(x []int, y int) []int {
+	var z []int
+	zlen := len(x) + 1
+	if zlen <= cap(x) {
+		// No room to grow; extend slice
+		z = x[:zlen]
+	} else {
+		// Insufficient space; allocate new array. Grow by doubling
+		zcap := zlen
+		if zcap < 2*len(x) {
+			zcap = 2 * len(x)
+		}
+		z = make([]int, zlen, zcap)
+		copy(z, x)
+	}
+	z[len(x)] = y
+	return z
+}
+
+func main() {
+	var x, y []int
+	for i := 0; i < 10; i++ {
+		y = appendInt(x, i)
+		fmt.Printf("%d cap: %d\t%v\n", i, cap(y), y)
+		x = y
+	}
+}
---
+++ goProgLang/04_compositeTypes/rev.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func reverse(s []int) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
+
+// Check if slices are equal
+func areEqual(x, y []string) bool {
+	if len(x) != len(y) {
+		return false
+	}
+	for i := range x {
+		if x[i] != y[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	a := [...]int{0, 1, 2, 3, 4, 5}
+	reverse(a[:])
+	fmt.Println(a) // 5, 4, 3, 2, 1, 0
+
+	// Rotate array
+	s := []int{0, 1, 2, 3, 4, 5}
+	reverse(s[:2])
+	reverse(s[2:])
+	reverse(s)
+	fmt.Println(s) // 2, 3, 4, 5, 0, 1
+}
