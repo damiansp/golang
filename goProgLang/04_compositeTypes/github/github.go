888991041f5dbe550d66c58d67477ade2757291d
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-05-14 08:33:56 -0700

    +github and issues*

---
+++ goProgLang/04_compositeTypes/github/github.go
@@ -0,0 +1,56 @@
+// Package github provides a Go API for the github issue tracker.
+// See https://developer.github.com/v3/search/#search-issues
+package github
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+)
+
+const IssuesURL = "https://api.github.com/search/issues"
+
+type IssuesSearchResult struct {
+	TotalCount int `json:"total_count"`
+	Items      []*Issue
+}
+
+type Issue struct {
+	Number    int
+	HTMLURL   string `json:"html_url"`
+	Title     string
+	State     string
+	User      *User
+	CreatedAt time.Time `json:"created_at"`
+	Body      string    // in markdown
+}
+
+type User struct {
+	Login   string
+	HTMLURL string `json:"html_url"`
+}
+
+// SearchIssues queries the Github issue tracker
+func SearchIssues(terms []string) (*IssuesSearchResult, error) {
+	q := url.QueryEscape(strings.Join(terms, " "))
+	resp, err := http.Get(IssuesURL + "?q=" + q)
+	if err != nil {
+		return nil, err
+	}
+
+	// Must close resp.Body on all execution paths
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, err
+	}
+
+	var result IssuesSearchResult
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		resp.Body.Close()
+		return nil, err
+	}
+	resp.Body.Close()
+	return &result, nil
+}
