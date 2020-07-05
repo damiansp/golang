bf824c4c0231505169a34e591af2dbb1bf1a94cb
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-07-04 21:28:56 -0700

    +Adress

--- headfirst/08_structs/main.go
+++ headfirst/08_structs/main.go
@@ -14,7 +14,11 @@ func main() {
 	fmt.Println(employee.Salary)
 
 	subscriber := magazine.Subscriber{Name: "Aman Singh", Rate: 4.99, Active: true}
+	address := magazine.Address{
+		Street: "123 Elm St.", City: "Elgin", State: "IL", PostalCode: "61407"}
+	subscriber.Address = address
 	fmt.Println("Name:", subscriber.Name)
 	fmt.Println("Rate:", subscriber.Rate)
 	fmt.Println("Active:", subscriber.Active)
+	fmt.Println(subscriber.Address)
 }
