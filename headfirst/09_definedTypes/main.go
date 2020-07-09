c96ac9ee4e1541ee2d81216932c0c9021be4789b
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-07-08 20:13:26 -0700

    main++

--- headfirst/09_definedTypes/main.go
+++ headfirst/09_definedTypes/main.go
@@ -7,7 +7,18 @@ type Gallons float64
 
 func main() {
 	var usFuel Gallons
-	usFuel = Gallons(10.)
-	euroFuel := Liters(240.)
-	fmt.Println(usFuel, euroFuel)
+	usFuel = Gallons(1.2)
+	euroFuel := Liters(4.5)
+	usFuel += liters2Gallons(Liters(40.))
+	euroFuel += gallons2Liters(Gallons(30.))
+	fmt.Printf("US Fuel: %0.1f gallons\n", usFuel)
+	fmt.Printf("Euro Fuel: %0.1f liters\n", euroFuel)
+}
+
+func liters2Gallons(liters Liters) Gallons {
+	return Gallons(liters * 0.264)
+}
+
+func gallons2Liters(gallons Gallons) Liters {
+	return Liters(gallons * 3.785)
 }
